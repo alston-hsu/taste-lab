16b3c82b8c1966757fcf4515e909fde5a3510303
ahsu <alston.hsu@outlook.com>
2025-01-14 17:00:00 -0600

    fetch food data by category added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './components/Navbar';
+import { getFoodByCategory } from '../services/foodService';
 import { ThemeProvider, createTheme, Theme } from '@mui/material/styles';
 import { Container } from '@mui/material';
 
 const theme: Theme = createTheme(); 
 
 function App() {
+  const [food, setFood] = useState([]);
+
+  useEffect(() => {
+    getFoodByCategory();
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <Navbar />
@@ -14,7 +21,7 @@ function App() {
         component="main"
         sx={{ display: 'flex', flexDirection: 'column', my: 16, gap: 4 }}
       >
-        <p>test</p>
+        <p>foodService</p>
       </Container>
     </ThemeProvider>
   );
